e500c94fc9170d2a00ba0382bc53473d1a4806ac
love2jn <love2jn@dukes.jmu.edu>
2015-03-02 15:17:50 -0500

    No compile errors

--- Lab4Act/app/src/main/java/com/example/doug/lab4act/Lab4Activity.java
+++ Lab4Act/app/src/main/java/com/example/doug/lab4act/Lab4Activity.java
@@ -1,67 +1,44 @@
 package com.example.doug.lab4act;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.example.doug.lab4act.view.SlidingTabLayout;
 
 import java.util.ArrayList;
 
+import com.example.doug.lab4act.view.SlidingTabLayout;
 
-public class Lab4Activity extends FragmentActivity implements  {
-
+public class Lab4Activity extends FragmentActivity {
     private SlidingTabLayout slidingTabLayout;
     private ViewPager viewPager;
     private ArrayList<Fragment> fragments;
-    private Lab4TabsViewPagerAdapter viewPagerAdapter;
+    private Lab4TabsViewPagerAdapter myViewPageAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab4);
 
-        //define sliding layout
+        // Define SlidingTabLayout (shown at top)
+        // and ViewPager (shown at bottom) in the layout.
+        // Get their instances.
         slidingTabLayout = (SlidingTabLayout) findViewById(R.id.tab);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        fragments = new ArrayList<Fragment>();
-        fragments.add(new Start());
+        // create a fragment list in order.
+        fragments = new ArrayList<>();
         fragments.add(new History());
         fragments.add(new Settings());
+        fragments.add(new Start());
 
-        //set things up with the sliding view
-        viewPagerAdapter =new Lab4TabsViewPagerAdapter(getFragmentManager(),
-                fragments);
-        viewPager.setAdapter(viewPagerAdapter);
+        // use FragmentPagerAdapter to bind the slidingTabLayout (tabs with different titles)
+        // and ViewPager (different pages of fragment) together.
+        myViewPageAdapter = new Lab4TabsViewPagerAdapter(getSupportFragmentManager(), fragments);
+        viewPager.setAdapter(myViewPageAdapter);
+
+        // make sure the tabs are equally spaced.
         slidingTabLayout.setDistributeEvenly(true);
         slidingTabLayout.setViewPager(viewPager);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lab4, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- Lab4Act/app/src/main/java/com/example/doug/lab4act/Lab4TabsViewPagerAdapter.java
+++ Lab4Act/app/src/main/java/com/example/doug/lab4act/Lab4TabsViewPagerAdapter.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 
 /**
  * Created by Fanglin Chen on 12/18/14.
- * Adapted by Douglas Applegate 3/2/2015
  */
 
 public class Lab4TabsViewPagerAdapter extends FragmentPagerAdapter {
@@ -48,3 +47,53 @@ public class Lab4TabsViewPagerAdapter extends FragmentPagerAdapter {
         return null;
     }
 }
+
+
+/*import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Fanglin Chen on 12/18/14.
+ * Adapted by Douglas Applegate 3/2/2015
+ */
+
+/*public class Lab4TabsViewPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragments;
+
+    public static final int ACTION = 0;
+    public static final int HISTORY = 1;
+    public static final int SETTINGS = 2;
+    public static final String UI_TAB_ACTION = "ACTION";
+    public static final String UI_TAB_HISTORY = "HISTORY";
+    public static final String UI_TAB_SETTINGS = "SETTINGS";
+
+    public Lab4TabsViewPagerAdapter(FragmentManager fm, ArrayList<Fragment> fragments){
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    public Fragment getItem(int pos){
+        return fragments.get(pos);
+    }
+
+    public int getCount(){
+        return fragments.size();
+    }
+
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case ACTION:
+                return UI_TAB_ACTION;
+            case HISTORY:
+                return UI_TAB_HISTORY;
+            case SETTINGS:
+                return UI_TAB_SETTINGS;
+            default:
+                break;
+        }
+        return null;
+    }
+}*/
